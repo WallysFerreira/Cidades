8529d2a20746241300987ca8b7d0474c9fc11d3e
Wallys Ferreira <wallysfsilva@gmail.com>
2023-08-18 16:20:44 -0300

    usar node-fetch pra fazer requisicao

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const CLIENT_ID = process.env.CLIENT_ID;
 const settings = {
 	method: 'GET',
 };
 
-fetch(`https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}`, settings)
+fetch(`https://api.unsplash.com/search/photos?per_page=1&query=recife&client_id=Gnyk3zT5LxqqYaOq3XbDbQVgz4W6uZWzRm4wQQF9gjI`, settings)
 	.then(res => res.json())
 	.then((json) => {
-		console.log(JSON.stringify(json));
+		console.log(json);
 	});
